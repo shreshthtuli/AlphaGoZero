f279c9c08a90c86594524839fd57e59b6120216c
Rithannen <soumyasharma98@gmail.com>
2019-10-26 00:29:43 +0530

    interactive vis added

--- vis.py
+++ vis.py
@@ -4,6 +4,11 @@ import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 
+"""
+Press c to move back, v to move forward
+Usage: python vis.py [filename]
+"""
+
 def plot_grid(rows, cols):
 	G = nx.grid_2d_graph(rows, cols)
 	pos = dict((n,n) for n in G.nodes())
@@ -17,26 +22,43 @@ def plot_stones_color(locs, clr):
 			if locs[i][j] == 0:
 				G.remove_node((i,j))
 	pos = dict((n,n) for n in G.nodes())
-	nodes = nx.draw_networkx_nodes(G, pos=pos, node_color=clr, node_size=460)
-	nodes.set_edgecolor('k')
+	nodes = nx.draw_networkx_nodes(G, pos=pos, node_color=clr, node_size=400)
+	if nodes:
+		nodes.set_edgecolor('k')
 	
-def draw_ith_row_df(df, i, board_shape):
-	data = df["States"].iloc[i]
-	data = ast.literal_eval(data)
-	print(data)
-	
-	# plot_stones_color(np.array(data[0]), 'k')
-	# plot_stones_color(np.array(data[8]), 'w')
+def draw_ith_row_df(fig, data, i):
+	state = data.iloc[i][0]
+	plot_grid(*board_shape)
+	plot_stones_color(np.array(state[0]), 'k')
+	plot_stones_color(np.array(state[8]), 'w')
+
+def press(event):
+	global curr_index, max_data, data, fig, ax
+	if event.key == "c" and curr_index>0:
+		curr_index-=1
+	elif event.key == "v" and curr_index<max_data:
+		curr_index+=1
+	# print("pressed", event.key, curr_index)
+	if event.key in ["c", "v"]:
+		ax.cla()
+		ax.set_facecolor('wheat')
+		draw_ith_row_df(fig, data, curr_index)
+		fig.canvas.draw()
 	
 BOARDSIZE = 13	
+curr_index = 0
 board_shape = (BOARDSIZE, BOARDSIZE)
-
 
 fig, ax = plt.subplots(nrows=1, ncols=1)
 ax.set_facecolor('wheat')
-plot_grid(*board_shape)
 
-df = pd.read_csv(sys.argv[1])
-draw_ith_row_df(df, 0, board_shape)
+max_data = 0
+fig.canvas.mpl_connect('key_press_event', press)
 
-# plt.show()
+df = pd.read_pickle(sys.argv[1])
+data = df["States"]
+max_data = df.shape[0]-1
+totaldata = len(data)
+draw_ith_row_df(fig, data, curr_index)
+
+plt.show()
